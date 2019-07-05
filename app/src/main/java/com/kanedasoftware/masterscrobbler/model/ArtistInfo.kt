1bebffd843db64f6df297e91f32d9bfaacadc649
kaneda_tetsuo <kaneda.software@gmail.com>
2019-07-04 22:57:19 -0300

    Validação das músicas e log em disco pra testes

---
+++ app/src/main/java/com/kanedasoftware/masterscrobbler/model/ArtistInfo.kt
@@ -0,0 +1,54 @@
+package com.kanedasoftware.masterscrobbler.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ArtistInfo(
+    val results: Results = Results()
+) {
+    data class Results(
+        @SerializedName("opensearch:Query")
+        val opensearchQuery: OpensearchQuery = OpensearchQuery(),
+        @SerializedName("opensearch:totalResults")
+        val opensearchTotalResults: String = "",
+        @SerializedName("opensearch:startIndex")
+        val opensearchStartIndex: String = "",
+        @SerializedName("opensearch:itemsPerPage")
+        val opensearchItemsPerPage: String = "",
+        val artistmatches: Artistmatches = Artistmatches(),
+        @SerializedName("@attr")
+        val attr: Attr = Attr()
+    ) {
+        data class Artistmatches(
+            val artist: List<Artist> = listOf()
+        ) {
+            data class Artist(
+                val name: String = "",
+                val listeners: String = "",
+                val mbid: String = "",
+                val url: String = "",
+                val streamable: String = "",
+                val image: List<Image> = listOf()
+            ) {
+                data class Image(
+                    @SerializedName("#text")
+                    val text: String = "",
+                    val size: String = ""
+                )
+            }
+        }
+
+        data class OpensearchQuery(
+            @SerializedName("#text")
+            val text: String = "",
+            val role: String = "",
+            val searchTerms: String = "",
+            val startPage: String = ""
+        )
+
+        data class Attr(
+            @SerializedName("for")
+            val forX: String = ""
+        )
+    }
+}
--- app/src/main/java/com/kanedasoftware/masterscrobbler/utils/Utils.kt
+++ app/src/main/java/com/kanedasoftware/masterscrobbler/utils/Utils.kt
@@ -10,19 +10,28 @@ import android.support.v4.app.NotificationManagerCompat
 import android.support.v7.app.AlertDialog
 import com.kanedasoftware.masterscrobbler.R
 import org.jetbrains.anko.*
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class Utils {
+
     companion object {
+
+        private const val DEBUG = true
+
         fun getSig(params: Map<String, String>, method: String): String {
             val hashmapParams = HashMap<String, String>(params)
             hashmapParams["api_key"] = Constants.API_KEY
             hashmapParams["method"] = method
             val sortedMap = hashmapParams.toSortedMap()
-
-
             var sigFormat = ""
             sortedMap.forEach { param ->
                 sigFormat = sigFormat.plus(param.key).plus(param.value)
@@ -35,21 +44,33 @@ class Utils {
         fun logDebug(message: String) {
             val log = AnkoLogger(Constants.LOG_TAG)
             log.debug(message)
+            if(DEBUG){
+                appendLog("[DEBUG] - $message")
+            }
         }
 
         fun log(message: String) {
             val log = AnkoLogger(Constants.LOG_TAG)
             log.info(message)
+            if(DEBUG){
+                appendLog("[INFO] - $message")
+            }
         }
 
         fun logWarning(message: String) {
             val log = AnkoLogger(Constants.LOG_TAG)
             log.warn(message)
+            if(DEBUG){
+                appendLog("[WARN] - $message")
+            }
         }
 
         fun logError(message: String) {
             val log = AnkoLogger(Constants.LOG_TAG)
             log.error(message)
+            if(DEBUG){
+                appendLog("[ERROR] - $message")
+            }
         }
 
         fun verifyNotificationAccess(context: Context) = NotificationManagerCompat.getEnabledListenerPackages(context).contains("com.kanedasoftware.masterscrobbler")
@@ -90,6 +111,32 @@ class Utils {
             val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val activeNetwork = cm.activeNetworkInfo
             return activeNetwork != null
+        }
+
+        fun appendLog(text: String) {
+            val logFile = File("sdcard/MasterScrobbler/MasterScrobbler.log")
+            val sdf = SimpleDateFormat("dd/MM/yyyy hh:mm:ss", Locale.ENGLISH)
+            val currentDate = sdf.format(Date())
+            if (!logFile.exists()) {
+                try {
+                    logFile.createNewFile()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+            var buf: BufferedWriter? = null
+            try {
+                buf = BufferedWriter(FileWriter(logFile, true))
+                buf.append("$currentDate - $text")
+                buf.newLine()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                if (buf != null) {
+                    buf.flush()
+                    buf.close()
+                }
+            }
         }
     }
 }
