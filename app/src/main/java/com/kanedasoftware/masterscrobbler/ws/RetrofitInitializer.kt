b96469f10179b1e7a27447b5f31875d4a4dcb4bc
kaneda_tetsuo <kaneda.software@gmail.com>
2019-07-12 14:41:59 -0300

    Busca das imagens utilizando bing.net

--- app/src/main/java/com/kanedasoftware/masterscrobbler/ws/RetrofitInitializer.kt
+++ app/src/main/java/com/kanedasoftware/masterscrobbler/ws/RetrofitInitializer.kt
@@ -1,6 +1,5 @@
 package com.kanedasoftware.masterscrobbler.ws
 
-import com.kanedasoftware.masterscrobbler.services.FanArtTvService
 import com.kanedasoftware.masterscrobbler.services.LastFmSecureService
 import com.kanedasoftware.masterscrobbler.services.LastFmService
 import retrofit2.Retrofit
@@ -18,14 +17,7 @@ class RetrofitInitializer {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-    private val fanArtTv = Retrofit.Builder()
-            .baseUrl("https://webservice.fanart.tv/v3/music/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
     fun lastFmService():LastFmService = lastFm.create(LastFmService::class.java)
 
     fun lastFmSecureService(): LastFmSecureService = secureLastFm.create(LastFmSecureService::class.java)
-
-    fun fanArtTvService(): FanArtTvService = fanArtTv.create(FanArtTvService::class.java)
 }
