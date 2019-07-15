5d7ab7a08f7738e25120cc13ca99036246caf10d
cardoso-thiago <thiago.cardoso33@gmail.com>
2019-07-14 22:45:04 -0300

    Inclusão dos textos no gridview

--- app/src/main/java/com/kanedasoftware/masterscrobbler/adapters/GridViewTopAdapter.kt
+++ app/src/main/java/com/kanedasoftware/masterscrobbler/adapters/GridViewTopAdapter.kt
@@ -1,45 +1,70 @@
 package com.kanedasoftware.masterscrobbler.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView.ScaleType.CENTER_CROP
+import android.widget.TextView
 import com.kanedasoftware.masterscrobbler.R
+import com.kanedasoftware.masterscrobbler.beans.TopBean
+import com.kanedasoftware.masterscrobbler.enums.EnumTopType
 import com.kanedasoftware.masterscrobbler.picasso.SquaredImageView
 import com.squareup.picasso.Picasso
 import java.util.*
 
+internal class GridViewTopAdapter(private val context: Context, beanList: ArrayList<TopBean>, type: EnumTopType) : BaseAdapter() {
 
-internal class GridViewTopAdapter(private val context: Context, imageUrls: ArrayList<String>) : BaseAdapter() {
-
-    private val urls = imageUrls
+    private val list = beanList
+    private val gridType = type
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-        var view: SquaredImageView? = if (convertView == null) {
-            SquaredImageView(context)
+        var gridViewAndroid: View
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        if (convertView == null) {
+            if (gridType == EnumTopType.ARTIST) {
+                gridViewAndroid = inflater.inflate(R.layout.gridview_artist_layout, null)
+                val textArtist = gridViewAndroid.findViewById<View>(R.id.gridview_artist_text) as TextView
+                textArtist.text = list[position].text1
+
+                val textPlays = gridViewAndroid.findViewById<View>(R.id.gridview_artist_plays) as TextView
+                textPlays.text = list[position].text2
+
+                val imageViewAndroid = gridViewAndroid.findViewById<View>(R.id.gridview_artist_image) as SquaredImageView
+                val url = list[position].url
+                if (!url.isBlank()) {
+                    Picasso.get().load(url).placeholder(R.drawable.placeholder).error(R.drawable.placeholder).fit().tag(context).into(imageViewAndroid)
+                }
+            } else {
+                gridViewAndroid = inflater.inflate(R.layout.gridview_album_layout, null)
+                val textAlbum = gridViewAndroid.findViewById<View>(R.id.gridview_album_text) as TextView
+                textAlbum.text = list[position].text1
+
+                val textAlbumArtist = gridViewAndroid.findViewById<View>(R.id.gridview_album_artist) as TextView
+                textAlbumArtist.text = list[position].text2
+
+                val textAlbumPlays = gridViewAndroid.findViewById<View>(R.id.gridview_album_plays) as TextView
+                textAlbumPlays.text = list[position].text3
+
+                val imageViewAndroid = gridViewAndroid.findViewById<View>(R.id.gridview_album_image) as SquaredImageView
+                val url = list[position].url
+                if (!url.isBlank()) {
+                    Picasso.get().load(url).placeholder(R.drawable.placeholder).error(R.drawable.placeholder).fit().tag(context).into(imageViewAndroid)
+                }
+            }
         } else {
-            convertView as SquaredImageView
+            gridViewAndroid = convertView
         }
 
-        view?.scaleType = CENTER_CROP
-        // Get the image URL for the current position.
-        val url = getItem(position)
-        if(url.isBlank()){
-            //TODO melhorar o placeholder, error e imagem inexistente
-            Picasso.get().load(R.drawable.placeholder).fit().tag(context).into(view)
-        } else {
-            Picasso.get().load(url).placeholder(R.drawable.placeholder).error(R.drawable.placeholder).fit().tag(context).into(view)
-        }
-        return view
+        return gridViewAndroid
     }
 
     override fun getCount(): Int {
-        return urls.size
+        return list.size
     }
 
-    override fun getItem(position: Int): String {
-        return urls[position]
+    override fun getItem(position: Int): String? {
+        return null
     }
 
     override fun getItemId(position: Int): Long {
---
+++ app/src/main/java/com/kanedasoftware/masterscrobbler/enums/EnumTopType.kt
@@ -0,0 +1,6 @@
+package com.kanedasoftware.masterscrobbler.enums
+
+enum class EnumTopType(var value: String) {
+    ARTIST("artist"),
+    ALBUM("album")
+}
