760260a2b7cf8739883b7f8fbf2491efe4249537
kaneda_tetsuo <kaneda.software@gmail.com>
2019-07-18 16:18:41 -0300

    Implementação do refresh

--- app/src/main/java/com/kanedasoftware/masterscrobbler/main/MainActivity.kt
+++ app/src/main/java/com/kanedasoftware/masterscrobbler/main/MainActivity.kt
@@ -37,9 +37,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            initSpinners()
+            getRecentTracks()
         }
         initService()
         getSessionKey()
